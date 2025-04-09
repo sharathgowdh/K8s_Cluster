70738a7bff848ad3748783ca62bbaed4c7cb1177
sharath.gowdah <sgowdah@netapp.com>
2025-04-09 12:38:11 +0530

    fixing the error

--- providers.tf
+++ providers.tf
@@ -1,10 +1,14 @@
 terraform {
+  required_version = ">= 1.3.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.47.0" # or whatever version you want to pin to
+      version = ">= 5.74.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 3.1.0, != 4.0.0"
     }
   }
-
-  required_version = ">= 1.3.0"
 }
